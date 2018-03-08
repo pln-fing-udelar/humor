3570820767731b16bd8b661daccdd07007815e17
Santiago Castro <santi.1410@hotmail.com>
2018-03-08 00:22:44 -0300

    Add definit dump

--- extraction/filter.py
+++ extraction/filter.py
@@ -7,6 +7,7 @@ LIMIT = 12000
 
 
 def main():
+    # FIXME: We should remove repeated tweets as well, as the hose can yield them.
     for tweet in random.sample(util.read_tweets_from_input(), LIMIT):
         print(tweet)
 

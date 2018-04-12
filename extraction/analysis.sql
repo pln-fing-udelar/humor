235d3fef3905df35ad5dc3484dfcc1bcc86ae663
Santiago Castro <sacastro@fing.edu.uy>
2018-04-12 19:23:29 -0300

    Add histogram analysis in a sql script

--- extraction/analysis.sql
+++ extraction/analysis.sql
@@ -28,3 +28,37 @@ WHERE tweet_id = 965857626843172864
       OR tweet_id = 965758586747047936
       OR tweet_id = 301481614033170432
       OR tweet_id = 968699034540978176;
+
+SELECT
+  votes,
+  COUNT(*) tweet_count
+FROM
+  (SELECT
+     COUNT(*) votes
+   FROM tweets t LEFT JOIN good_quality gq on (t.tweet_id = gq.tweet_id)
+   GROUP BY t.tweet_id) temp
+GROUP BY votes;
+
+SELECT
+  tweet_id                                                                AS id,
+  REPLACE(text, '"', '""')                                                AS text,
+  p / c > 0.5                                                             AS is_humor,
+  x                                                                       AS votes_no,
+  v1                                                                      AS votes_1,
+  v2                                                                      AS votes_2,
+  v3                                                                      AS votes_3,
+  v4                                                                      AS votes_4,
+  v5                                                                      AS votes_5,
+  IF(p / c > 0.5, (v1 * 1 + v2 * 2 + v3 * 3 + v4 * 4 + v5 * 5) / p, NULL) AS funniness_average
+FROM (SELECT
+        tweet_id,
+        COUNT(IF(vote = '1', 1, NULL))  v1,
+        COUNT(IF(vote = '2', 1, NULL))  v2,
+        COUNT(IF(vote = '3', 1, NULL))  v3,
+        COUNT(IF(vote = '4', 1, NULL))  v4,
+        COUNT(IF(vote = '5', 1, NULL))  v5,
+        COUNT(IF(vote = 'x', 1, NULL))  x,
+        COUNT(IF(vote != 'x', 1, NULL)) p,
+        COUNT(*)                        c
+      FROM good_quality) temp NATURAL JOIN tweets
+ORDER BY is_humor DESC, RAND();
